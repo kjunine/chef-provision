a17b88a57deb12c5bbb90a7badab24e8e9c0843a
Daniel Ku <kjunine@gmail.com>
2014-10-26 04:31:32 +0900

    Adds initial files.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'provision'
+maintainer       'Daniel Ku'
+maintainer_email 'kjunine@gmail.com'
+license          'MIT'
+description      'Installs/Configures provision'
+long_description 'Installs/Configures provision'
+version          '0.1.0'
+
